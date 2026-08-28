3230fbf6b8d0e9b5b1a741e132bfe4c8095e1546
Philip <philiptoshevski@gmail.com>
2026-08-28 22:52:48 +0200

    Remove unused vaccine view models

--- ViewModels/VaccineDisplayViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VeterinaryClinic.ViewModels
-{
-    public class VaccineDisplayViewModel
-    {
-        public int Id { get; set; }  // Vaccine ID
-        public string Name { get; set; } = string.Empty;
-        public DateTime DateAdministered { get; set; }
-    }
-}
